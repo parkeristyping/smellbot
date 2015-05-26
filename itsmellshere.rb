c6f78dcd7f664cc52370d90f141d025bf7c518ea
Parker Lawrence <parker.alford@gmail.com>
2015-05-26 15:12:58 -0400

    finalize updates to permit posts to Rails app

--- itsmellshere.rb
+++ itsmellshere.rb
@@ -34,29 +34,6 @@ no_update
 # here's a list of things to exclude from searches
 #exclude "hi", "spammer", "junk"
 
-
-# TODO: Get time at initialization
-
-loop do
-  # search through all replies (TODO: since last check)
-  replies do |tweet|
-    # if tweet does not have geolocation, reply with further instructions if tweet does not have geolocation
-    if !tweet.geo?
-      # reply "Thanks, but you need to enable location services and 'share precise location'. More detailed instructions here: www.placeholder.com"
-    else
-      binding.pry
-      post(tweet: tweet, user: tweet.user)
-      # reply "Thanks! Your smell has been added to our database. Check out the map at www.itsmellshere.com"
-    end
-  end
-
-  # sleep 60 seconds
-  sleep 60
-
-  # TODO: update time
-end
-
-
 def post(post_url: "/smells", tweet:, user:)
 
   body = {
@@ -76,6 +53,42 @@ def post(post_url: "/smells", tweet:, user:)
 
   req = Net::HTTP::Post.new(post_url, initheader = {'Content-Type' =>'application/json'})
   req.body = body
-  Net::HTTP.new("localhost", "3000").start {|http| http.request(req) }
+  Net::HTTP.new("localhost", "3000").start {|http| http.request(req)}
+
+end
+
+loop do
+  # TODO: Set last update based on most recent smell in database (from Rails app)
+  last_update ||= nil
+
+  first_time_through = true
+
+  # search through all replies
+  replies do |tweet|
+    # End if we've gotten into old tweets
+    break if last_update && tweet.created_at < last_update
+
+    # if tweet does not have geolocation, reply with further instructions
+    if !tweet.geo?
+      if first_time_through
+        this_update = tweet.created_at
+        first_time_through = false
+      end
+      reply "Thanks, but you need to enable location services and 'share precise location'. More detailed instructions here: www.placeholder.com", tweet
+    else
+      if first_time_through
+        this_update = tweet.created_at
+        first_time_through = false
+      end
+      binding.pry
+      post(tweet: tweet, user: tweet.user)
+      reply "Thanks! Your smell has been added to our database. Check out the map at www.itsmellshere.com", tweet
+    end
+  end
+
+  last_update = this_update
+
+  # sleep 60 seconds
+  sleep 60
 
 end
