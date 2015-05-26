16fa973e7275a79c1f3584a7b41e7b5002f96bf0
Parker Lawrence <parker.alford@gmail.com>
2015-05-26 14:52:12 -0400

    add post method

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem "pry"
 gem "chatterbot", "~> 0.9.3"
+gem "json"
--- itsmellshere.rb
+++ itsmellshere.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/env ruby
 
 require 'bundler'
-require 'rubygems'
 require 'yaml'
 require 'chatterbot/dsl'
-
+require 'net/http'
 Bundler.require(:default)
+
+file = YAML.load_file('itsmellshere.yml')
+
+consumer_key file[:consumer_key]
+consumer_secret file[:consumer_secret]
+
+secret file[:secret]
+token file[:token]
 
 #
 # this is the script for the twitter bot itsmellshere
@@ -13,25 +20,62 @@ Bundler.require(:default)
 #
 
 # remove this to send out tweets
-debug_mode
+#debug_mode
 
 # remove this to update the db
 no_update
 
 # remove this to get less output when running
-verbose
+#verbose
 
 # here's a list of users to ignore
-blacklist "abc", "def"
+#blacklist "abc", "def"
 
 # here's a list of things to exclude from searches
-exclude "hi", "spammer", "junk"
+#exclude "hi", "spammer", "junk"
 
-search "keyword" do |tweet|
- binding.pry
- reply "Hey #USER# nice to meet you!", tweet
+
+# TODO: Get time at initialization
+
+loop do
+  # search through all replies (TODO: since last check)
+  replies do |tweet|
+    # if tweet does not have geolocation, reply with further instructions if tweet does not have geolocation
+    if !tweet.geo?
+      # reply "Thanks, but you need to enable location services and 'share precise location'. More detailed instructions here: www.placeholder.com"
+    else
+      binding.pry
+      post(tweet: tweet, user: tweet.user)
+      # reply "Thanks! Your smell has been added to our database. Check out the map at www.itsmellshere.com"
+    end
+  end
+
+  # sleep 60 seconds
+  sleep 60
+
+  # TODO: update time
 end
 
-replies do |tweet|
-  reply "Yes #USER#, you are very kind to say that!", tweet
+
+def post(post_url: "/smells", tweet:, user:)
+
+  body = {
+    "smell" => {
+      "smell" => {
+        "content" => tweet.text,
+        "lat" => tweet.geo.coordinates[0],
+        "lng" => tweet.geo.coordinates[1]
+      },
+      "user" => {
+        "twitter_id" => user.id,
+        "twitter_handle" => user.handle,
+        "name" => user.name
+      }
+    }
+  }.to_json
+
+  req = Net::HTTP::Post.new(post_url, initheader = {'Content-Type' =>'application/json'})
+  req.body = body
+  Net::HTTP.new("localhost", "3000").start {|http| http.request(req) }
+
 end
