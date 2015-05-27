f33409170dd8a533cd80de7e2578fead1c97e8f7
Parker Lawrence <parker.alford@gmail.com>
2015-05-27 19:46:20 -0400

    try to make heroku-ready

--- itsmellshere.rb
+++ itsmellshere.rb
@@ -6,21 +6,11 @@ require 'chatterbot/dsl'
 require 'net/http'
 Bundler.require(:default)
 
-file = YAML.load_file('itsmellshere.yml')
+consumer_key ENV[:consumer_key]
+consumer_secret ENV[:consumer_secret]
 
-consumer_key file[:consumer_key]
-consumer_secret file[:consumer_secret]
-
-secret file[:secret]
-token file[:token]
-
-#
-# this is the script for the twitter bot itsmellshere
-# generated on 2015-05-26 12:14:04 -0400
-#
-
-# remove this to update the db
-no_update
+secret ENV[:secret]
+token ENV[:token]
 
 class SmellBot
 
@@ -33,40 +23,16 @@ class SmellBot
     stream
   end
 
-  private
-  def get_last_update_time
-    response = Net::HTTP.start("www.itsmellshere.com", "80") do |json|
-      request = Net::HTTP::Get.new "/smells/last.json"
-      JSON.parse((json.request request).body)
-    end
-    Time.parse(response["created_at"])
-  end
-
-  def check_since_last_update
-    replies do |tweet|
-      break if tweet.created_at <= @last_update
-      handle tweet
-    end
-  end
-
-  def stream
-    streaming do
-      replies do |tweet|
-        handle tweet
-      end
-    end
-  end
-
-  def handle(tweet)
+  def self.handle(tweet)
     if !tweet.geo?
       reply "#USER# That stinks! You need to enable location services. Instructions: www.itsmellshere.com/enable_location", tweet
     else
-      post_smell(tweet: tweet, user: tweet.user)
+      self.post_smell(tweet: tweet, user: tweet.user)
       reply "#USER# Thanks! Your smell was added. Check out the map at www.itsmellshere.com", tweet
     end
   end
 
-  def post_smell(post_url: "/smells", tweet:, user:)
+  def self.post_smell(post_url: "/smells", tweet:, user:)
     body = {
       "smell" => {
         "content" => tweet.text,
@@ -82,6 +48,30 @@ class SmellBot
     req = Net::HTTP::Post.new(post_url, initheader = {'Content-Type' =>'application/json'})
     req.body = body
     Net::HTTP.new("www.itsmellshere.com", "80").start {|http| http.request(req)}
+  end
+
+  private
+  def get_last_update_time
+    response = Net::HTTP.start("www.itsmellshere.com", "80") do |json|
+      request = Net::HTTP::Get.new "/smells/last.json"
+      JSON.parse((json.request request).body)
+    end
+    Time.parse(response["created_at"])
+  end
+
+  def check_since_last_update
+    replies do |tweet|
+      break if tweet.created_at <= @last_update
+      SmellBot::handle tweet
+    end
+  end
+
+  def stream
+    streaming do
+      replies do |tweet|
+        SmellBot::handle tweet
+      end
+    end
   end
 end
 
